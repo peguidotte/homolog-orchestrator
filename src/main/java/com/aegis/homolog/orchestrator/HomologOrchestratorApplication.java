812e44ecfa0f3f579c9abe654abdc2b508b16bb2
Pedro Guidotte <peguidotte@gmail.com>
2025-10-22 21:01:10 -0300

    Git init

---
+++ src/main/java/com/aegis/homolog/orchestrator/HomologOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.aegis.homolog.orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomologOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomologOrchestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aegis/homolog/orchestrator/HomologOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aegis.homolog.orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomologOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
